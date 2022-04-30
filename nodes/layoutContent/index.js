381e9f8f975b4c0e5cee6596e6685f808af6295d
Mr. Lin <cufoon@qq.com>
2022-05-01 02:19:16 +0800

    feat: 添加漏掉的LayoutContent

---
+++ nodes/layoutContent/index.js
@@ -0,0 +1,30 @@
+const { checkElements, checkElementsMap, mapArrayAdd } = require('../util');
+
+module.exports = function (RED) {
+  function LowerCaseNode(config) {
+    RED.nodes.createNode(this, config);
+    const id = config.cufoon_id;
+    const parent = config.cufoon_parent;
+    const defaultContent = config.cufoon_default_content;
+    var node = this;
+    node.on('input', function (msg) {
+      let parent1 = parent;
+      if (parent !== '') {
+        msg.payload = id;
+      } else {
+        parent1 = msg.payload;
+      }
+      msg = checkElements(msg);
+      msg.cufoon.elements.set(id, {
+        id: id,
+        name: 'layoutContent',
+        content: { text: defaultContent || '' }
+      });
+      msg = checkElementsMap(msg);
+      mapArrayAdd(msg.cufoon.elementsMap, parent1, id);
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType('layoutContent', LowerCaseNode);
+};
